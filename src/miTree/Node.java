061c21d216bfec4fe88420a26cec49159d1a7e0f
kozer1223 <kozerskilo@interia.pl>
2016-06-07 10:13:13 +0200

    po co boolean isLeaf?
    Będzie można przecież instanceof zrobić.

--- src/miTree/Node.java
+++ src/miTree/Node.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class Node<K extends Comparable<K>, V> {
 	
-	//TODO wkleiæ tutaj BPT
+	//TODO wkleiÃ¦ tutaj BPT
 	public List<K> keys;
 	public List<V> values;
-	private final boolean isLeaf;
+	private final boolean isLeaf; //po co?
 	
 	public Node(int order, boolean isLeaf){
-		this.isLeaf = isLeaf;
+		this.isLeaf = isLeaf; //?????
 		keys = new ArrayList<K>(order);
 		values = new ArrayList<V>(order + 1);
 	}
